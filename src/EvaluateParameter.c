d521fa322df5ed703c00b6a69fb1e97ab12799da
bingxuan3133 <bensoncbx@gmail.com>
2014-03-22 02:04:35 +0800

    Add test_EvaluateParameter_Partial_Integration.c
    Use the real stringObject instead of mocking
    while shuntingYard remains mocking
    
    All tests passed

--- src/EvaluateParameter.c
+++ src/EvaluateParameter.c
@@ -10,15 +10,16 @@ Argument *evaluate1to2parameter(String *parameter) {
 	
 	subString = getWordAndUpdate(parameter, ",;");
 	if(subString->length != 0) {
-		buffer = stringCopyX(subString, 0, subString->length);
+		buffer = stringCopy(subString, 0, subString->length);
 		argument->operand1 = shuntingYard(buffer);
 	} else {
 		argument->operand1 = -1;
 	}
 	
+	stringLeftTrim(parameter);
 	subString = getWordAndUpdate(parameter, ",;");
 	if(subString->length != 0) {
-		buffer = stringCopyX(subString, 0, subString->length);
+		buffer = stringCopy(subString, 0, subString->length);
 		argument->operand2 = shuntingYard(buffer);
 	} else {
 		argument->operand2 = -1;
--- test/test_EvaluateParameter_Partial_Integration.c
+++ test/test_EvaluateParameter_Partial_Integration.c
@@ -1,6 +1,6 @@
 #include "unity.h"
 #include "EvaluateParameter.h"
-#include "mock_StringObject.h"
+#include "StringObject.h"
 #include "mock_Token.h"
 #include <malloc.h>
 
@@ -15,16 +15,9 @@ void tearDown(void)
 void test_evaluate1to2parameter_should_pass_one_parameter_from_rawString_to_argument () {
 	Argument *argument;
 	String parameter = {.rawString = "movwf 0x10" , .startIndex = 6, .length = 4};
-	String argument1 = {.rawString = "movwf 0x10" , .startIndex = 6, .length = 4};
-	String argument2 = {.rawString = "movwf 0x10" , .startIndex = 10, .length = 0};
-	char* buffer = malloc(1);
+	char *buffer = "0x10";
 	
-	getWordAndUpdate_ExpectAndReturn(&parameter, ",;", &argument1);
-	stringCopy_ExpectAndReturn(&argument1, 0, argument1.length, buffer);
 	shuntingYard_ExpectAndReturn(buffer, 0x10);
-	
-	stringLeftTrim_Expect(&parameter);
-	getWordAndUpdate_ExpectAndReturn(&parameter, ",;", &argument2);
 	
 	argument = evaluate1to2parameter(&parameter);
 	
@@ -39,17 +32,12 @@ void test_evaluate1to2parameter_should_pass_one_parameter_from_rawString_to_argu
 void test_evaluate1to2parameter_should_pass_two_parameter_from_rawString_to_argument () {
 	Argument *argument;
 	String parameter = {.rawString = "movwf 0x10, ACCESS" , .startIndex = 6, .length = 12};
-	String argument1 = {.rawString = "movwf 0x10, ACCESS" , .startIndex = 6, .length = 4};
-	String argument2 = {.rawString = "movwf 0x10, ACCESS" , .startIndex = 12, .length = 6};
-	char* buffer = malloc(1);
+	char *buffer = "0x10";
 	
-	getWordAndUpdate_ExpectAndReturn(&parameter, ",;", &argument1);
-	stringCopy_ExpectAndReturn(&argument1, 0, argument1.length, buffer);
 	shuntingYard_ExpectAndReturn(buffer, 0x10);
 	
-	stringLeftTrim_Expect(&parameter);
-	getWordAndUpdate_ExpectAndReturn(&parameter, ",;", &argument2);
-	stringCopy_ExpectAndReturn(&argument2, 0, argument2.length, buffer);
+	buffer = "ACCESS";
+	
 	shuntingYard_ExpectAndReturn(buffer, -5);
 	
 	argument = evaluate1to2parameter(&parameter);
